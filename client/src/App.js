402d62ba7832f7da301e2d28993c56ea70d149b5
Anected <anected@gmail.com>
2019-02-03 13:21:04 +0300

    'adding'

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,11 @@ class App extends Component {
     state = {
         name: null,
         age: null,
-        intervalIsSet: false
+        users:[]
     };
 
     componentDidMount() {
-        if (!this.state.intervalIsSet) {
-            let interval = setInterval(this.getDataFromDb, 1000);
-            this.setState({intervalIsSet: interval});
-        }
+        this.getDataFromDb();
     }
 
     getDataFromDb = () => {
@@ -24,51 +21,59 @@ class App extends Component {
     };
 
     putDataToDB = (userName, userAge) => {
-        axios.post("http://localhost:3001/api/users", {
-            name: userName,
-            age: userAge
-        });
+        if (userName != null && userAge != null){
+            axios.post("http://localhost:3001/api/users", {
+                name: userName,
+                age: userAge
+            })
+                .then(res => {
+                    return this.getDataFromDb()
+                })
+        }
     };
-
 
     render() {
         const {users} = this.state;
-        console.log(users);
         return (
             <div className="App">
-                <table className={"table_user"}>
-                    <caption>Users</caption>
-                    <tr className={'tr_user'}>
-                        <th className={'th_user'}>id</th>
-                        <th className={'th_user'}>name</th>
-                        <th className={'th_user'}>age</th>
+                <table className={"container"}>
+                    <caption>Пользователи</caption>
+                    <thead>
+                    <tr >
+                        <th>Id</th>
+                        <th>Имя</th>
+                        <th >Возраст</th>
                     </tr>
+                    </thead>
+                    <tbody>
                     {users.map((user, key) => {
                         const {_id, name, age} = user;
                         return (
-                            <tr key={key} className="tr_user">
-                                <td className={"td_user"}>{_id}</td>
-                                <td className={"td_user"}>{name}</td>
-                                <td className={"td_user"}>{age}</td>
+                            <tr onClick =key={key} >
+                                <td >{_id}</td>
+                                <td>{name}</td>
+                                <td >{age}</td>
                             </tr>
                         )
                     })}
+                    </tbody>
                 </table>
-                <div style={{padding: "10px"}}>
+                <div>
                     <input
                         type="text"
                         onChange={e => this.setState({name: e.target.value})}
-                        placeholder="add name"
-                        style={{width: "200px"}}
+                        placeholder="Введите имя"
+                        className="input"
                     />
                     <input
                         type="text"
                         onChange={e => this.setState({age: e.target.value})}
-                        placeholder="add age"
-                        style={{width: "200px"}}
+                        placeholder="Введите возраст"
+                        className="input"
+
                     />
-                    <button onClick={() => this.putDataToDB(this.state.name, this.state.age)}>
-                        ADD
+                    <button className="button" onClick={() => this.putDataToDB(this.state.name, this.state.age)}>
+                        Добавить пользователя
                     </button>
                 </div>
             </div>
